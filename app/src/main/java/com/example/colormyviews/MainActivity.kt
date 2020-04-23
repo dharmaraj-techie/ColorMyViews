a84b31ece635981f345662eeba742935bdc4bfef
Dharmaraj <dharmaraj.techie@gmail.com>
2020-04-23 21:02:04 +0530

    add chain btns

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +17,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun setClickListeners() {
         val clickableViews: List<View> =
-            listOf(box_1_tv, box_2_tv, box_3, box_4, box_5, constraint_layout)
+            listOf(
+                box_1_tv,
+                box_2_tv,
+                box_3,
+                box_4,
+                box_5,
+                constraint_layout,
+                red_btn,
+                green_btn,
+                yellow_btn
+            )
 
         for (item in clickableViews) {
             item.setOnClickListener() { makeColored(item) }
@@ -35,6 +46,10 @@ class MainActivity : AppCompatActivity() {
             R.id.box_3 -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_4 -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_5 -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_btn -> box_3.setBackgroundResource(android.R.color.holo_red_dark)
+            R.id.yellow_btn -> box_4.setBackgroundResource(R.color.yellow_color)
+            R.id.green_btn -> box_5.setBackgroundResource(R.color.green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
